d4b217ead202f9a5b744ab67ad71226473495b39
ahr <abdulhafizr>
2021-02-26 22:45:59 +0700

    | Setup express-js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello Express js");
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.get('/', (request, response) => {
+    response.send("Home server");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`);
+})
