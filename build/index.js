1db5a4125e2f3cce278202089a56a6140b70c583
ahr <abdulhafizr>
2021-02-26 23:22:27 +0700

    | Change setup express to typescript

--- build/index.js
+++ build/index.js
@@ -4,11 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app = express_1.default();
-const PORT = 8000;
-app.get('/', (request, response) => {
-    response.send("Home server");
-});
+const PORT = 4000;
+class App {
+    constructor() {
+        this.routes = () => {
+            this.app.get("/", (request, response) => {
+                response.send("Dashboard");
+            });
+        };
+        this.app = express_1.default();
+        this.routes();
+    }
+}
+const app = new App().app;
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
 
-const app = express();
-const PORT = 8000;
+const PORT = 4000;
 
-app.get('/', (request, response) => {
-    response.send("Home server");
-})
+class App {
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+        this.routes();
+    }
+
+    protected routes = () : void => {
+        this.app.get("/", (request: Request, response: Response) => {
+            response.send("Dashboard");
+        })
+    } 
+}
+
+const app = new App().app;
 
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`);
