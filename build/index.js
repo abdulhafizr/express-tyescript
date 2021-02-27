8fc66b8ea183768b8f77622adebbeba397816374
ahr <abdulhafizr>
2021-02-27 08:07:59 +0700

    | Add Package compression, helmet, cors

--- build/index.js
+++ build/index.js
@@ -5,6 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const compression_1 = __importDefault(require("compression"));
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
 const PORT = 4000;
 class App {
     constructor() {
@@ -12,6 +15,9 @@ class App {
             this.app.use(morgan_1.default("dev"));
             this.app.use(express_1.default.json());
             this.app.use(express_1.default.urlencoded({ extended: true }));
+            this.app.use(compression_1.default());
+            this.app.use(helmet_1.default());
+            this.app.use(cors_1.default());
         };
         this.routes = () => {
             this.app.get("/", (request, response) => {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import morgan from 'morgan';
+import compression from 'compression';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const PORT = 4000;
 
@@ -16,6 +19,9 @@ class App {
         this.app.use(morgan("dev"));
         this.app.use(express.json());
         this.app.use(express.urlencoded({extended: true}));
+        this.app.use(compression());
+        this.app.use(helmet());
+        this.app.use(cors());
     }
 
     protected routes = () : void => {
